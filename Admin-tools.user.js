ab306fba41e158f64c0439c1d1f5a2d49cb8ad60
abigplant <98791291+abigplant@users.noreply.github.com>
2024-02-02 09:33:06 -0800

    Update Admin-tools.user.js
    Added substring selector to address constantly changing class names for the container class. This should reduce how often this breaks.

--- Admin-tools.user.js
+++ Admin-tools.user.js
@@ -4,9 +4,9 @@
 // @include     https://admin.shopify.com/store/*
 // @include     https://*.myshopify.com/admin*
 // @grant       GM_addStyle
-// @version     1.1
+// @version     1.5
 // @author      Graham Connell
 //
 // @description Hides support and developer tools from the bottom left of the admin while viewing as a support advisor. Hover and scroll down over the settings button to reveal settings
 // ==/UserScript==
-GM_addStyle('._StickyBottomNav_1fe9e_3 {height: 4.3em; overflow: hidden;} ._StickyBottomNav_1fe9e_3:hover {overflow: visible;}');
+GM_addStyle('[class*="StickyBottomNav"], .vNwaY {height: 4.3em; overflow: hidden;} [class*="StickyBottomNav"]:hover, .vNwaY:hover {overflow: visible;}');
